23e2302f6d89c1c9c9c75f8e19ed45e8036effc2
heewoon <heewoon@DESKTOP-2STJNIR>
2020-06-02 17:55:40 +0900

    111

--- freehoon/src/main/java/com/freehoon/web/HomeController.java
+++ freehoon/src/main/java/com/freehoon/web/HomeController.java
@@ -4,12 +4,19 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.freehoon.web.board.model.BoardVO;
+import com.freehoon.web.board.model.MyStudent;
 
 /**
  * Handles requests for the application home page.
@@ -18,25 +25,49 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model, HttpServletRequest req, @RequestParam("name") String name) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		
 		System.out.println("### formattedDate--->"+ formattedDate);
+
+		logger.info("request.getParameter(\'id\')------{}.", req.getParameter("id"));
+		logger.info("@RequestParam('name')------{}.", name);
 		
 		model.addAttribute("serverTime", formattedDate );
 		model.addAttribute("hello", "Hello Java Spring World !!!" );
 		
 		return "home";
+	}
+	
+	@ModelAttribute("value2")
+	public String myview(Model model) {
+		return "pass to view";
+	}
+	@ModelAttribute("value3")
+	public BoardVO boardList() {
+		BoardVO boardVO = new BoardVO();
+		boardVO.setTitle("≈∏¿Ã∆≤");
+		boardVO.setCate_cd("222222222222");
+		return boardVO;
+	}
+	
+	@RequestMapping(value = "value3")
+	public String myview2() {
+		return "board/index";
+	}
+	
+	@RequestMapping(value = "myview")
+	public String myview(MyStudent student) {
+		return "test/myView";
 	}
 	
 }
--- freehoon/src/main/webapp/WEB-INF/views/home.jsp
+++ freehoon/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,3 +1,4 @@
+<%@page import="com.freehoon.web.board.model.BoardVO"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" %>
 <html>
@@ -10,5 +11,17 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+<%= request.getAttribute("serverTime") %><p>
+<%= request.getAttribute("value2") %><p>
+<c:out value="${value2}"/><p>
+<c:out value="<font color='blue'>${hello}</font>" escapeXml="false"/><br><br>
+
+<% 
+	BoardVO boardVO = (BoardVO) request.getAttribute("value3"); 
+	out.println(boardVO.getTitle()+"<br>");
+	out.println(boardVO.getCate_cd()+"<br>");
+%>
+<%-- <%= boardVO.getTitle() %><br><br>
+<%= boardVO.getCate_cd() %><br><br> --%>
 </body>
 </html>
