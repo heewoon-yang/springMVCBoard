e9f6c3a2758a8e9ed6fcd333b38bb613e9b0a0b9
heewoon <heewoon@DESKTOP-2STJNIR>
2020-06-03 10:25:29 +0900

    1

---
+++ freehoon/src/main/java/com/freehoon/web/board/model/MyStudentCommand.java
@@ -0,0 +1,22 @@
+package com.freehoon.web.board.model;
+
+public class MyStudentCommand {
+	private int age;
+	private String name;
+	
+	public MyStudentCommand() {}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
